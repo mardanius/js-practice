5a0022d7637ed927b722dacf1317bdcf2f50600e
mardanius <mariojeda.mg@gmail.com>
2022-07-08 08:42:43 -0400

    Slice and Spread Operator

--- js-practice.js
+++ js-practice.js
@@ -445,16 +445,18 @@ let numbers = [0,1,2,3,4,5,666,'fsddf'];
 numbers.splice(6,2,6,7,8,9);
 console.log(numbers);
 
-// Slice to extract values from an array and create a new one.
+// Slice and Spread Operator
 
 let temp = ['cool', 'warm', 'sunny', 'windy'];
 
 function summerInCali(temp) {
     let summerInCali = temp.slice(1,3);
-    return summerInCali;
+    let allYearTemp = ['Impredecible',...temp];
+    return `Summer ${summerInCali} All year ${allYearTemp}`;
 }
 
 console.log(summerInCali(temp));
+
 
 
 
